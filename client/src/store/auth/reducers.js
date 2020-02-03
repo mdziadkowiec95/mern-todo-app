a7f0241f17deb2cd8caddb55eff006a396bb14a4
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-02-03 23:25:39 +0100

    Improve UI and refactor backend part
    - Improve styles of email confirmation related components
    - Move nodemailer "sendMail" method to separate/reusable functions
    - Change the flow of SignUp component to inform user about sending confirmation email instead of redirecting to SignIn

--- client/src/store/auth/reducers.js
+++ client/src/store/auth/reducers.js
@@ -25,6 +25,7 @@ export default function(state = initialState, action) {
         isLoading: true,
       }
       case AuthActionTypes.REGISTER_USER_SUCCESS:
+      case AuthActionTypes.RESEND_CONFIRM_EMAIL_ERROR:
         return {
           ...state,
           isLoading: false,
