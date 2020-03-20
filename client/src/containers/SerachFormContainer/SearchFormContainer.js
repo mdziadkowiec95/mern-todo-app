852e1da0e16d2af3388310af78e6fa659c705d9d
Michal Dziadkowiec <michal.dziadkowiec@motorolasolutions.com>
2020-03-20 16:36:58 +0100

    SearchForm component continuation
    - fix styles
    - implement linking to page for specific task
    - fix serachForm logic

--- client/src/containers/SerachFormContainer/SearchFormContainer.js
+++ client/src/containers/SerachFormContainer/SearchFormContainer.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import SearchForm from '../../components/molecules/SearchForm/SearchForm'
+import { quickSearch } from '../../store/search/thunks'
 
 class SearchFormContainer extends Component {
   render() {
+    const { quickSearch, suggestions, isLoading } = this.props
+
     return (
       <>
-        <SearchForm />
+        <SearchForm quickSearch={quickSearch} suggestions={suggestions} isLoading={isLoading} />
       </>
     )
   }
 }
 
-export default SearchFormContainer
+const mapDispatchToProps = dispatch => bindActionCreators({ quickSearch }, dispatch)
+const mapStateToProps = ({ search }) => ({
+  suggestions: search.searchResults,
+  isLoading: search.isLoading,
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchFormContainer)
